887ef9cede620e16cf2ce7f06e6985ca542125d8
Garett Brock <gbrock16@gmail.com>
2019-06-27 23:48:15 -0700

    through Ch.4.9 - database migrations and querying using SQLAlchemy

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABSE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = (os.environ.get('DATABSE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')) + '?check_same_thread=False'
     SQLALCHEMY_TRACK_MODIFICATION = False
